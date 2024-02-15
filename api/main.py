2fe07c1baabbb0a361007ea62d4b61bb556549a7
sebastian <sebnarvaez19@gmail.com>
2024-02-15 09:38:24 -0500

    feat: update hero function

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, HTTPException, Query
 from sqlmodel import Session, select
 
 from .database import create_db_and_tables, engine
-from .models import Hero, HeroCreate, HeroRead
+from .models import Hero, HeroCreate, HeroRead, HeroUpdate
 
 
 app = FastAPI()
@@ -42,3 +42,21 @@ def read_hero(hero_id: int):
             raise HTTPException(status_code=404, detail="Hero not found")
 
         return hero
+
+
+@app.patch("/heroes/{hero_id}", response_model=HeroRead)
+def update_hero(hero_id: int, hero: HeroUpdate):
+    with Session(engine) as session:
+        db_hero = session.get(Hero, hero_id)
+        if not db_hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+
+        hero_data = hero.model_dump(exclude_unset=True)
+        for key, value in hero_data.items():
+            setattr(db_hero, key, value)
+
+        session.add(db_hero)
+        session.commit()
+        session.refresh(db_hero)
+
+        return db_hero
