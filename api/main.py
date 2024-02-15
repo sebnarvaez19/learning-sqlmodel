773b594653601bf582cb737d184b6779cd47a09f
sebastian <sebnarvaez19@gmail.com>
2024-02-15 09:19:38 -0500

    feat: function to get one hero

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from sqlmodel import Session, select
 
 from .database import create_db_and_tables, engine
@@ -32,3 +32,13 @@ def read_heroes():
         heroes = session.exec(select(Hero)).all()
 
         return heroes
+
+
+@app.get("/heroes/{hero_id}", response_model=HeroRead)
+def read_hero(hero_id: int):
+    with Session(engine) as session:
+        hero = session.get(Hero, hero_id)
+        if not hero:
+            raise HTTPException(status_code=404, detail="Hero not found")
+
+        return hero
