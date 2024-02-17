9bb928d988f59c0fb54edca4e4989be0dbc35a4c
sebastian <sebnarvaez19@gmail.com>
2024-02-16 23:40:20 -0500

    feat: add connection CORS to task app

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from typing import List
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import Session, select
 
 from .database import create_db_and_tables, engine
@@ -17,10 +18,26 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 async def get_session():
     with Session(engine) as session:
         yield session
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hi, go to docs to test the API"}
 
 
 @app.post("/tasks/", response_model=TaskRead)
